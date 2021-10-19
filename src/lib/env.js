7d2cf491c07f13a722a017e933cab1c29b731b0c
Thiago Lagden <lagden@gmail.com>
2021-10-19 12:11:29 -0300

    update packs

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,7 +1,8 @@
 import envs from '../../resource/env.js'
 
 // Cache das informações armazenadas no dataset
-const _data = new Map()
+// const _data = new Map()
+const _data = new Set()
 
 /**
  * Carrega os dados definidos como variáveis de ambiente
@@ -15,19 +16,22 @@ export function getEnv() {
  * Retorna todos os dados definidos no data-* do elemento <script>
  * @return {object} Retorna um objeto
  */
-export function getData() {
-	const {TARGET_JS} = getEnv()
-	if (_data.has(TARGET_JS)) {
-		return _data.get(TARGET_JS)
+export function getData(t) {
+	if (_data.size === 1) {
+		return [..._data][0]
 	}
-	const el = document.getElementById(TARGET_JS)
+
+	const {TARGET_JS} = getEnv()
+	const id = t ?? TARGET_JS
+
+	const el = globalThis.document.getElementById(id)
 	const o = {}
 	if (el) {
 		for (const [key, value] of Object.entries(el.dataset)) {
 			o[key] = value
 		}
 	}
-	_data.set(TARGET_JS, o)
+	_data.add(o)
 	return o
 }
 
@@ -35,7 +39,7 @@ export function getData() {
  * Define onde aplicação será renderizada
  * @return {HTMLElement} Retorna o elemento
  */
-export function getTarget() {
-	const data = getData()
-	return document.getElementById(data.target) ?? document.body
+export function getTarget(t) {
+	const data = getData(t)
+	return globalThis.document.getElementById(data.target) ?? globalThis.document.body
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import {getTarget, getEnv} from './lib/env'
+import {getTarget, getEnv} from './lib/env.js'
 
 import App from './_components/App.svelte'
 
-const target = getTarget()
+const TARGET_JS = new URL(import.meta.url).searchParams.get('TARGET_JS')
+const target = getTarget(TARGET_JS)
 const envs = getEnv()
 
+console.debug('TARGET_JS via import.meta.url', TARGET_JS)
 console.debug('envs', envs)
 console.info('ヾ(⌐■_■)ノ ....')
 
