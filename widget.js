1bb740aa5745c870d4893daf52e5aab004ea02b5
lagden <lagden@gmail.com>
2023-11-29 00:52:17 +0000

    Deploying to gh-pages from @ lagden/boilerplate-svelte@f1572e66c41c52c0de00a3050f28d85dab007236 🚀

--- widget.js
+++ widget.js
@@ -1,7 +1,11 @@
-import {getURL} from 'https://service.teleport.com.br/cors/v1/load/?url=https://unpkg.com/@tadashi/common@3.2.0/src/lib/url/get-url.js'
-import {getProp} from 'https://service.teleport.com.br/cors/v1/load/?url=https://unpkg.com/@tadashi/common@3.2.0/src/lib/dom/prop.js'
-import {rnd as uuid} from 'https://service.teleport.com.br/cors/v1/load/?url=https://unpkg.com/@tadashi/common@3.2.0/src/lib/rnd.js'
-import {createElement} from 'https://service.teleport.com.br/cors/v1/load/?url=https://unpkg.com/@tadashi/common@3.2.0/src/lib/create-element.js'
+// @ts-ignore
+import {getURL} from 'https://unpkg.com/@tadashi/common/src/lib/url/get-url.js'
+// @ts-ignore
+import {getProp} from 'https://unpkg.com/@tadashi/common/src/lib/url/prop.js'
+// @ts-ignore
+import {rnd as uuid} from 'https://unpkg.com/@tadashi/common/src/lib/rnd.js'
+// @ts-ignore
+import {createElement} from 'https://unpkg.com/@tadashi/common/src/lib/create-element.js'
 
 function create(props, elementType) {
 	const element = createElement(elementType, props)
@@ -12,7 +16,7 @@ function create(props, elementType) {
 async function load() {
 	const rnd = uuid()
 	const url = getURL(import.meta.url)
-	const id = getProp(import.meta.url) ?? 'boilerplate_svelte_js'
+	const id = getProp(import.meta.url, 'id') ?? 'boilerplate_svelte_js'
 
 	// Informações dos pacotes
 	const response = await fetch(`${url}/files.json?noCache=${rnd}`)
@@ -27,20 +31,28 @@ async function load() {
 	// Load CSS
 	const cssFile = files?.['style.css']?.file ?? false
 	if (cssFile) {
-		create({
-			href: `${url}/${cssFile}?noCache=${rnd}`,
-			rel: 'stylesheet',
-			type: 'text/css',
-		}, 'link')
+		create(
+			{
+				href: `${url}/${cssFile}?noCache=${rnd}`,
+				rel: 'stylesheet',
+				type: 'text/css',
+				crossOrigin: 'anonymous',
+			},
+			'link',
+		)
 	}
 
 	// Load script
 	const script = files?.['index.html']
 	if (script?.isEntry === true) {
-		create({
-			src: `${url}/${script.file}?TARGET_JS=${id}&noCache=${rnd}`,
-			type: 'module',
-		}, 'script')
+		create(
+			{
+				src: `${url}/${script.file}?TARGET_JS=${id}&noCache=${rnd}`,
+				type: 'module',
+				crossOrigin: 'anonymous',
+			},
+			'script',
+		)
 	}
 }
 
